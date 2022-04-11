17892908eb5f274f8cbd597bacc0d99092d93bec
JP Mitra <johnpaulmitra@yahoo.com>
2022-04-10 21:09:27 -0500

    Anime Quote API

--- 2022-04-14 - APIs/Project 1/js/main.js
+++ 2022-04-14 - APIs/Project 1/js/main.js
@@ -2,15 +2,19 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  // const choice = document.querySelector('input').value
+  const url = 'https://animechan.vercel.app/api/random'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+        document.querySelector('.anime').innerText = data.anime
+        document.querySelector('.character').innerText = data.character
+        document.querySelector('.quote').innerText = data.quote
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
+
+getFetch()
