18929cd855567a1c6910db52ba7638df86ae6a61
JP Mitra <johnpaulmitra@yahoo.com>
2022-04-10 22:13:44 -0500

    Pokemon API

--- 2022-04-14 - APIs/Project 2/js/main.js
+++ 2022-04-14 - APIs/Project 2/js/main.js
@@ -2,15 +2,20 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
-
+  let choice = document.querySelector('input').value
+  const url = 'https://pokeapi.co/api/v2/pokemon/' + choice
+  
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('.name').innerText = data.species.name
+        document.querySelector('img').src = data.sprites.front_default
+        document.querySelector('.types').innerText = data.types[0].type.name + ', ' + (data.types[1] === undefined ? '' : data.types[1].type.name)
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
+
+getFetch()
