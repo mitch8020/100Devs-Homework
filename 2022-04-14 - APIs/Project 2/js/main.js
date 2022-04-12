3101b8744e1a1ad55e99d247e1fa6bb6ab4d573e
JP Mitra <johnpaulmitra@yahoo.com>
2022-04-12 17:09:15 -0500

    Pokemon API: Casing Update

--- 2022-04-14 - APIs/Project 2/js/main.js
+++ 2022-04-14 - APIs/Project 2/js/main.js
@@ -2,16 +2,16 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  let choice = document.querySelector('input').value
+  let choice = document.querySelector('input').value.toLowerCase()
   const url = 'https://pokeapi.co/api/v2/pokemon/' + choice
   
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('.name').innerText = data.species.name
+        document.querySelector('.name').innerText = data.species.name.toUpperCase()
         document.querySelector('img').src = data.sprites.front_default
-        document.querySelector('.types').innerText = data.types[0].type.name + ', ' + (data.types[1] === undefined ? '' : data.types[1].type.name)
+        document.querySelector('.types').innerText = (data.types[0].type.name + ' ' + (data.types[1] === undefined ? '' : data.types[1].type.name)).toUpperCase()
       })
       .catch(err => {
           console.log(`error ${err}`)
