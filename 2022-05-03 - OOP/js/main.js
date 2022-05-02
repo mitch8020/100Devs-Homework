f3f5cdc158eb4657287095dc8728338c0b8f8659
JP Mitra <johnpaulmitra@yahoo.com>
2022-05-01 22:23:34 -0500

    JS Way (Ch 9): Dogs

--- 2022-05-03 - OOP/js/main.js
+++ 2022-05-03 - OOP/js/main.js
@@ -1,1 +1,37 @@
+/*
+==========================
+Dogs
+==========================
+*/
+
+class Dog {
+  constructor(name, species, size) {
+    this.name = name
+    this.species = species
+    this.size = size
+  }
+  bark () {
+    if (this.size > 60) {
+      return 'Grrr! Grrr!'
+    } else {
+      return 'Woof! Woof!'
+    }
+  }
+}
+
+// TODO: define the Dog class here
+
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
+
+/*
+==========================
+Character inventory
+==========================
+*/
 
