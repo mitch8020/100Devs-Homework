71ec8a6f1b6dae4789bf28ceb99c7535fbaa78a3
JP Mitra <johnpaulmitra@yahoo.com>
2022-05-09 18:48:50 -0500

    Removed NASA API Link

--- 2022-04-14 - APIs/Project 3/js/main.js
+++ 2022-04-14 - APIs/Project 3/js/main.js
@@ -1,16 +1,52 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+let today = new Date()
+let localDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+document.querySelector('#get-image').addEventListener('click', determineDate)
+
+function getFetch() {
+  const url = '/* REMOVED NASA API LINK -- ADD BACK BEFORE USE */' + localDate
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      if (data.media_type === 'image') {
+        document.querySelector('img').src = data.url
+        document.querySelector('iframe').src = ""
+      } else if (data.media_type === 'video') {
+        document.querySelector('iframe').src = data.url
+        document.querySelector('img').src = ""
+      }
+      
+      document.querySelector('h2').innerText = data.title
+      document.querySelector('h3').innerText = data.date
+      document.querySelector('p').innerText = data.explanation
+      document.querySelector('small').innerText = data.copyright
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
+getFetch()
+
+function determineDate() {
+  localDate = document.querySelector('input').value
+  getFetch()
+}
+
+document.querySelector('#previous').addEventListener('click', previousImage)
+
+function previousImage() {
+  let tempDate = new Date(localDate)
+  localDate = `${tempDate.getFullYear()}-${tempDate.getMonth() + 1}-${tempDate.getDate() - 1}`
+  console.log(tempDate)
+  getFetch()
+}
+
+document.querySelector('#next').addEventListener('click', nextImage)
+
+function nextImage() {
+  let tempDate = new Date(localDate)
+  localDate = `${tempDate.getFullYear()}-${tempDate.getMonth() + 1}-${tempDate.getDate() + 1}`
+  getFetch()
 }
